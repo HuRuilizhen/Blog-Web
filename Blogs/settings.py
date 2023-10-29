3cc7ba6d1fa2c4639d18412350e7defec3e82b12
HuRuilizhen <122090168@link.cuhk.edu.cn>
2023-10-30 02:36:23 +0800

    version 0.4
    update:
    - beautify README.md
    - rewrite settings to start server on Internet  or LAN

--- Blogs/settings.py
+++ Blogs/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5k&zybd*ogb=hb()2dl(et&o3@^zz1jb!7z2xb$xdi-(%7kxnb"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
--- Blogs/urls.py
+++ Blogs/urls.py
@@ -13,11 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.views import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+
+
+from Blogs import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(("users.urls", "users"), namespace="users")),
     path("", include(("contents.urls", "contents"), namespace="contents")),
+    re_path(
+        r"^static/(?P<path>.*)$",
+        static.serve,
+        {"document_root": settings.STATIC_ROOT},
+        name="static",
+    ),
 ]
