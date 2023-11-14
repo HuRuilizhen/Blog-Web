c1f8aa3914a94befe6e6f4a5c6fc85f3fbdc62af
HuRuilizhen <122090168@link.cuhk.edu.cn>
2023-11-15 00:30:24 +0800

    version 0.5
    update:
    - support more Chinese
    - support Markdown in ckeditor
    - update time zone to shanghai

--- Blogs/settings.py
+++ Blogs/settings.py
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "zh-hans"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Asia/Shanghai"
 
 USE_I18N = True
 
@@ -142,25 +142,26 @@ CKEDITOR_CONFIGS = {
                 "Outdent",
                 "Indent",
                 "-",
-                "Blockquote",
-                "CodeSnippet",
-                "-",
                 "JustifyLeft",
                 "JustifyCenter",
                 "JustifyRight",
                 "JustifyBlock",
             ],
+            ["Link", "Unlink"],
             [
-                "Link",
                 "Image",
                 "Table",
                 "HorizontalRule",
                 "Smiley",
                 "SpecialChar",
+                "Blockquote",
+                "CodeSnippet",
+                "-",
+                "Markdown",
             ],
             ["Styles", "Format", "Font", "FontSize"],
         ],
-        "extraPlugins": ",".join(["codesnippet"]),
+        "extraPlugins": ",".join(["codesnippet", "markdown"]),
     },
     "comment": {
         "width": "1100px",
